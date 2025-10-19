c4fd17a341b1fd3226cdf23fce1133cb4f1bd8c4
User <User@DESKTOP-A62SU3A>
2025-10-19 23:44:13 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace Marginean_Silviu_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; } // cheia primară
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public ICollection<Book>? Books { get; set; } // navigation property către cărți
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace Marginean_Silviu_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+    }
+}

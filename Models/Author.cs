df99654c415ec400a832fedc0eb0eae8d81a001a
User <User@DESKTOP-A62SU3A>
2025-10-20 14:20:56 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Marginean_Silviu_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Navigation property: un autor poate avea mai multe cărți
+        public ICollection<Book>? Books { get; set; }
+    }
+}
